daac49c8570134c19ad2f037da5dc8424b687b4d
Jeff <rustyangel@gmail.com>
2025-07-28 12:30:28 -0600

    Initialise project

---
+++ languages/nunjucks/highlights.scm
@@ -0,0 +1,40 @@
+([
+  "{{"
+  "}}"
+  "{{-"
+  "-}}"
+  "{%"
+  "%}"
+  "{%-"
+  "-%}"
+] @tag.delimiter
+  (#set! priority 110))
+
+([
+  "asyncAll"
+  "asyncEach"
+  "endall"
+  "endeach"
+  "endfor"
+  "endmacro"
+  "for"
+  "macro"
+] @keyword
+  (#set! priority 110))
+
+([
+  "else"
+  "elif"
+  "elseif"
+  "endif"
+  "if"
+] @keyword.conditional
+  (#set! priority 110))
+
+([
+  "and"
+  "contains"
+  "in"
+  "or"
+] @keyword.operator
+  (#set! priority 110))
