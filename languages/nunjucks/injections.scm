feaac90279f3f4dbd5cfdc61001cfdf0f7d5fa7b
Jeff <rustyangel@gmail.com>
2026-02-15 21:48:05 -0700

    Add frontmatter support (#4)

--- languages/nunjucks/injections.scm
+++ languages/nunjucks/injections.scm
@@ -1,3 +1,6 @@
 ((content) @injection.content
-  (#set! injection.language "html")
-  (#set! injection.combined))
+	(#set! injection.language "html")
+	(#set! injection.combined))
+
+((front_matter) @injection.content
+	(#set! injection.language "yaml"))
